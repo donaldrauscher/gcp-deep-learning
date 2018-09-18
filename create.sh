ea7b15b4460c5289232e6124778fba7869c2cc89
Donald Rauscher <donald.rauscher@gmail.com>
2018-09-18 08:03:34 -0500

    Attach static IP to instance

--- create.sh
+++ create.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 JUPYTER_PW=${JUPYTER_PW:-password}
+REGION=$(gcloud config get-value compute/region)
+
 JUPYTER_RULE=$(gcloud compute firewall-rules list --filter "NAME=jupyter" --format "table(name)")
+JUPYTER_STATIC=$(gcloud compute addresses list --filter "NAME=jupyter" --format "table(name)")
 
 if [ -z "$JUPYTER_RULE" ]; then
     gcloud compute firewall-rules create "jupyter" \
@@ -14,7 +17,14 @@ if [ -z "$JUPYTER_RULE" ]; then
         --target-tags "jupyter"
 fi
 
+if [ -z "$JUPYTER_STATIC" ]; then
+    gcloud compute addresses create "jupyter" --region $REGION
+fi
+
+JUPYTER_ADDR=$(gcloud compute addresses describe "jupyter" --region $REGION --format "value(address)")
+
 gcloud beta compute instances create "deep-learning" \
+    --address "$JUPYTER_ADDR" \
     --machine-type "n1-standard-2" \
     --accelerator type=nvidia-tesla-p100,count=1 \
     --image-family "ubuntu-1604-lts" --image-project "ubuntu-os-cloud" \
